f99b8734f7b5726c574f24608d1ee7d953eb413f
Luke Powell <powell@kevinkelleyconcepts.com>
2025-06-09 14:13:20 -0500

    Installed dependencies and updated directory structure

---
+++ app/constants/index.ts
@@ -0,0 +1,6 @@
+export const appName = 'Kitchen + Kocktails USA'
+export const appDescription =
+  'Intellectual Property and Business Law Attorneys for High-Growth Startups, Scaling Companies, and Creatives Entrepreneurs.'
+export const appURL = 'https://kitchenkocktailsusa.com'
+export const twitterCard = ''
+export const ogImage = ''
